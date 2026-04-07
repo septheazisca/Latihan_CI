fc35f9d2acb6cb472c4a24d11c218c763d5fa929
septheazisca <septheaziscaaurora@gmail.com>
2026-04-07 10:40:57 +0700

    pertemuan 2: belajar segment

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/home/coba-parameter/(:alpha)/(:num)/(:alphanum)', 'Home::belajar_segment/$1/$2/$3');
---
+++ app/Views/segment_view.php
@@ -0,0 +1,3 @@
+Parameter 1 berupa alfabet adalah <?=  $p1; ?> <br />
+Parameter 2 berupa angka adalah <?=  $p2; ?>  <br />
+Parameter 3 berupa gabungan huruf dan angka adalah <?=  $p3; ?> 
